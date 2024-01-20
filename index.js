b1f9c3c004d3233c0244a5f06dc71f7745c61d76
labbeltran <labbeltran@gmail.com>
2024-01-20 01:35:46 -0500

    added controller

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
+require(`dotenv`).config() // code to add env files
 const express = require(`express`)
 const app = express()
+
+app.use(`/places`, require(`/controllers/places`)) //code to import controller; adds path `/places` to any other path defined in the controller
 
 app.get(`/`, (req, res)=>{
     res.send("hello")
 })
 
-app.listen(3000)
+app.get(`*`, (req,res)=>{
+    res.status(404).send(`<h1>Wow, lots of nothing</h1><a href="https://i.redd.it/3lsp5tedugt41.jpg"; alt="error 404 you wouldn't understand image">`)
+})
+
+app.listen(process.env.PORT)
