9c630846c003697b0118c8e93db54648a5019c22
labbeltran <labbeltran@gmail.com>
2024-01-21 14:46:17 -0500

    added ../

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.engine(`jsx`, require (`express-react-views`).createEngine())
 app.use(`/places`, require(`/controllers/places`)) //code to import controller; adds path `/places` to any other path defined in the controller
 
 app.get(`/`, (req, res)=>{
-    res.render(home)
+    res.render(`home`)
 })
 
 app.get(`*`, (req,res)=>{
