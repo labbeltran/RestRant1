d3ecb79633ff9f3225ad5e053c3e884df92b6ccf
labbeltran <labbeltran@gmail.com>
2024-01-24 22:56:46 -0500

    tried adding req.body & part 7 steps, but something went wrong

--- controllers/places.js
+++ controllers/places.js
@@ -1,4 +1,5 @@
 const router = require(`express`).Router()
+const places = require(`../models/places.js`)
 
 router.get(`/`, (req, res)=>{
     let places = [{
@@ -39,6 +40,21 @@ router.get('/new', (req, res) => {
 
 router.post('/', (req, res) => {
   console.log(req.body)
+  router.post('/', (req, res) => {
+    console.log(req.body)
+    if (!req.body.pic) {
+      // Default image if one is not provided
+      req.body.pic = 'http://placekitten.com/400/400'
+    }
+    if (!req.body.city) {
+      req.body.city = 'Anytown'
+    }
+    if (!req.body.state) {
+      req.body.state = 'USA'
+    }
+    places.push(req.body)
+    res.redirect(`/places`)
+  })
   res.send('POST /places stub')
 })
 
