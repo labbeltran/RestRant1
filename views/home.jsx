59dc20b4803ce40f669b3661c74082df8fe790e1
labbeltran <labbeltran@gmail.com>
2024-01-25 20:08:27 -0500

    added style to homepage

--- views/home.jsx
+++ views/home.jsx
@@ -5,15 +5,20 @@ function Home() {
     return (
       <Def>
         <main>
-          <h1>Welcome to the Home Page?!</h1>
+          <h1>Welcome, to the Cuisine Citadel</h1>
           <div id="tacos-div">
             <img src="/Images/tacos.jpg" alt="Tacos" id="tacos" className="tacos"/>
           </div>
           <a href="/places">
             <button className="btn-primary" type="submit">Places Page</button>
           </a>
-
-          <p>This is the main page of the site.</p>
+          <div className="home-opening">
+          <p>ようこそ, みんなさん. The cuisine citadel provides you with assistance 
+            when your stomach is rumbling,
+            but you don't know where to go to fulfill your sustenance needs.  </p>
+            
+          </div>
+          
         </main>
       </Def>
     );
