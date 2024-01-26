745aeb1c88cc2da5638e3e4b3e0bcad025c5fe61
labbeltran <labbeltran@gmail.com>
2024-01-25 20:58:29 -0500

    added delete form

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,11 +1,15 @@
 const React = require(`react`)
 const Def= require(`../default`)
 
-function show(){
+function show(data){
     return(
         <Def>
             <main>
-                <h1>Show Page</h1>
+                <h1>{data.place.name}</h1>
+                <a href="" className="btn btn-warning">Edit</a>
+                <form>
+                    <button type="submit" className="btn btn-danger">Delete</button>
+                </form>
             </main>
         </Def>
     )
