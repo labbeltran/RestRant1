097bc50a3fcfdd814ad510f7e39ba70c594a2dcf
labbeltran <labbeltran@gmail.com>
2024-02-04 13:42:26 -0500

    added error messages

--- controllers/places.js
+++ controllers/places.js
@@ -20,8 +20,11 @@ router.post('/', (req, res) => {
   .catch(err=>{
     if(err && err.name==`ValidationError`){
       let message= `Validation Error:`
-      //ToDo: Find All Validation Errors
-      res.render(`places/new`, {message})
+      for(var field in err.errors){
+        message+= `${field} was ${err.errors[field].value}.`
+        message+= `${err.errors[field].message}`
+      }
+      res.render(`new`, {message})
     }
     else{
       res.render(`error404`)}
